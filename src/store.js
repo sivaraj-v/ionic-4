451c13193ba46450dcb43f839e6d1940138b7b00
Sivaraj V <sivaraj.v@ef.com>
2019-03-13 19:54:08 +0530

    Cleanup react

--- src/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { indexReducer } from './reducer/index-reducer';
-const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
-export const store = createStore(indexReducer, composeEnhancers(applyMiddleware(thunk)));
